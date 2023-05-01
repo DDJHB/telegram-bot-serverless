8f3d457fddde14d80bfda1583711c7799723c6ac
Gunel Huseynli <ghuseynli8447@ada.edu.az>
2023-05-01 22:53:29 +0400

    wallet fix

--- src/constructor/payment_handlers/payment_utils.py
+++ src/constructor/payment_handlers/payment_utils.py
@@ -2,7 +2,7 @@ import json
 
 from web3 import Web3
 
-from src.constructor.web3_utils import send_transaction_to_contract, get_base_wallet_info, call_view_contract_method
+from src.constructor.web3_utils import send_transaction_to_contract, call_view_contract_method
 from src.database.eth_transactions import put_transaction_request
 from src.database.user_info import get_user_info_record
 
@@ -40,10 +40,10 @@ def deposit_to_contract(username: str, amount: float, chat_id: int, additional_i
 def transfer(passengers: list[str], username: str, amount: float, chat_id: int, additional_information: dict):
     function_name = "processPayment"
 
-    base_wallet_info = get_base_wallet_info()
+    wallet_info = get_user_info_record(username=username)
     receiver_wallet_info = get_user_info_record(username=username)
     tx_hash = send_transaction_to_contract(
-        wallet_info=base_wallet_info,
+        wallet_info=wallet_info,
         contract_name=contract_name,
         function_name=function_name,
         function_args=[passengers, receiver_wallet_info['wallet_address'], Web3.toWei(amount, 'finney')],
